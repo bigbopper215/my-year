233efa9211045f102753bfb2e1611a4a7d8c01e4
brandonliu <bliu@vassar.edu>
2025-01-03 15:22:47 -0800

    gsap partially implemented

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+ // use a script tag or an external JS file
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+  // gsap code here!
+  gsap.from('.jan-header', { 
+    scrollTrigger: {
+      trigger: '.jan-header',
+      toggleActions: 'restart none none none'
+    },
+    duration: 1, 
+    y: '100%' 
+  });
+});
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
@@ -11,5 +26,4 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
-
 
