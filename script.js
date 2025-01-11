e4d651ba68764acc0d2346b3f74d34cf7f5b1408
brandonliu <bliu@vassar.edu>
2025-01-10 16:53:34 -0800

    jan flashcards added

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
   // gsap code here!
   const wrappedText = new SplitType('#wrapped', { types: 'chars'})
 
+  gsap.to('.intro-img', {
+    filter: "blur(0px)", // End state
+    delay: 2,
+    x: "0%",
+    duration: 1,         // Animation duration
+    scrollTrigger: {
+      trigger: ".intro-img", // Element to trigger the animation
+      start: "top 80%",     // Trigger animation when image is 80% into view
+      end: "top 50%",       
+      scrub: true,   
+    }
+  });
+
   gsap.to(wrappedText.chars, {
     y: 0,
     stagger: 0.05, 
@@ -20,6 +33,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
     duration: 1, 
     y: '100%' 
   });
+
+  document.querySelectorAll('.flashcard').forEach(card => {
+    let isFlipped = false;
+  
+    card.addEventListener('click', () => {
+      const cardInner = card.querySelector('.card-inner');
+  
+      isFlipped = !isFlipped;
+      
+      gsap.to(cardInner, {
+        rotateY: isFlipped ? 180 : 0,
+        duration: 0.6,
+        ease: "power2.out"
+      });
+    });
+  });
+
 });
 
 const observer = new IntersectionObserver((entries) => {
@@ -35,4 +65,5 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
 
